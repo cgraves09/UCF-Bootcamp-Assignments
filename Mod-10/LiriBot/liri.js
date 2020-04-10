dc2b8ecba13f6e1c9f2315bcc1b7bb4bec340e24
cgraves09 <christopher.graves09@gmail.com>
2020-04-09 21:27:42 -0400

    added missing semicolons to spotify function

--- Mod-10/LiriBot/liri.js
+++ Mod-10/LiriBot/liri.js
@@ -1,11 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 const axios = require('axios');
 const spotifyNode = require('node-spotify-api');
 const moment = require('moment');
 const keys = require("./keys.js");
 const fs = require("fs");
-var bandApiKey = keys.bandsInTown.apiKey
-var movieApiKey = keys.ombd.apiKey
+var bandApiKey = keys.bandsInTown.apiKey;
+var movieApiKey = keys.ombd.apiKey;
 
 
 // GLOBAL VARIABLES 
@@ -22,18 +22,17 @@ if (options === "spotify-this-song") {
   };  
   argString = emptyArray.join('+');    
   thisSong();
-}
-
+};
 if (options === "concert-this"){
   for (var i = 3; i < arg.length; i++){
     emptyArray.push(arg[i]);
   };  
   argString = emptyArray.join('+');    
   concerts();
-}
+};
 if (options === "movie-this"){
   if(process.argv[3] === undefined){
-    argString = "mr+nobody"
+    argString = "mr+nobody";
     noMovie();
   }else{
     for (var i = 3; i < arg.length; i++){
@@ -43,19 +42,18 @@ if (options === "movie-this"){
     movies();    
   }
 
-}
+};
 if(options === "do-what-it-says"){
   fs.readFile("random.txt", "utf8", function(error, data) {
     if (error) {
     return console.log(error);
   }
-  
     var dataArr = data.split(",");
     options = dataArr[0];
-    argString = dataArr[1]
+    argString = dataArr[1];
     thisSong();
-  })
-}
+  });
+};
 
 // Spotify Api Function
 function thisSong(){
@@ -66,39 +64,39 @@ function thisSong(){
       
       var itemsArray = response.tracks.items;
       
-      for(var j = 0; j < itemsArray.length; j++ ){
+      for(var j = 0; j < itemsArray.length; j++){
         
-        for(var k = 0; k < itemsArray[j].artists.length; k++  ){
+        for(var k = 0; k < itemsArray[j].artists.length; k++){
           var artist = itemsArray[j].artists[k].name;
           var songName = itemsArray[j].name;
           var url = itemsArray[j].preview_url;
           var album = itemsArray[j].album.name;
           console.log("\nArtist Name: " + artist + "\n Song: " + songName + "\n Preview Url " + url + "\n Album: " + album + "\n"); 
-        }
-      }
+        };
+      };
       if(itemsArray[0]=== undefined){
         songString = "the+sign";
         spotifyLimit = 1;
         thisSong();
-      }
+      };
     })
     .catch(function(err) {
       console.log(err);
     });
-}
+};
 // Bands In Town Api Function
 function concerts(){
   axios.get("https://rest.bandsintown.com/artists/" + argString + "/events?app_id=" + bandApiKey)
   .then(function(response){
-    var data = response.data
+    var data = response.data;
     for (var i = 0; i < data.length; i++){
-      var venue = data[i].venue.name
+      var venue = data[i].venue.name;
       var location = data[i].venue.city + " " + data[i].venue.region;
       var formatDate = moment(data[i].datetime).format("dddd, MMMM Do YYYY, h:mm");
       console.log("\nVenue: " + venue + "\n Location: " + location + "\n Time: " + formatDate + "\n");
-    }
-  })
-}
+    };
+  });
+};
 // OMB Api Function
 function movies(){
   axios.get("http://www.omdbapi.com/?t=" + argString + "&apikey=" + movieApiKey)
@@ -112,12 +110,12 @@ function movies(){
     var language = data.Language;
     var plot = data.Plot;
     var actors = data.Actors;
+
     console.log("\nTitle: " + title + "\n Year: " + year + "\n Imdb Rating: " + imdbRating + "\n Rotten Tomatos Rating: "
      + rtRating + "\n Country Produced: " + country + "\n Language: " + language + "\n Plot: " + plot + "\n Actors: " + actors + "\n");
-    
-  })
-}
-
+  });
+};
+//  If no movie title is passed through
 function noMovie (){
   axios.get("http://www.omdbapi.com/?t=" + argString + "&apikey=trilogy")
     .then(function(response){
@@ -129,10 +127,10 @@ function noMovie (){
       var language = data.Language;
       var plot = data.Plot;
       var actors = data.Actors;
+
       console.log("\nTitle: " + title + "\n Year: " + year + "\n Imdb Rating: " + imdbRating
         + "\n Country Produced: " + country + "\n Language: " + language + "\n Plot: " + plot + "\n Actors: " + actors + "\n");
-    
-  })  
-}
+  });  
+};
 
 

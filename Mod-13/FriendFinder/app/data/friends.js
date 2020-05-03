78367f5eea895a2c285e597a945eb0d69223f27b
Chris Graves <chrisgraves@Chriss-MacBook-Pro.local>
2020-05-03 17:08:57 -0400

    test upload

--- Mod-13/FriendFinder/app/data/friends.js
+++ Mod-13/FriendFinder/app/data/friends.js
@@ -1,6 +1,6 @@
 let friendsData = [
     {
-        name: "Chris",
+        name: "Joe Exotic",
         photo: 'https://imagez.tmz.com/image/90/4by3/2020/04/07/904b148d83a1446ea8e01824f94f1cce_md.jpg',
         scores: [2,4,3,5,2,1,4,2,4,3]
     }
--- Mod-13/FriendFinder/app/server.js
+++ Mod-13/FriendFinder/app/server.js
@@ -1,11 +1,11 @@
 let express = require('express');
-let path = require('path');
 
 let app = express();
 let PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 require('./routing/apiRoutes')(app);
 require('./routing/htmlRoutes')(app);
